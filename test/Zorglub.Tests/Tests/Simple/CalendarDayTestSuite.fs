176485ec9c78edd33a5ddab8d92478704d757e92
Chtoucas <chtoucas@narvalo.org>
2022-05-13 09:23:57 +0200

    Test all calendar types.

--- test/Zorglub.Tests/Tests/Simple/CalendarDayTestSuite.fs
+++ test/Zorglub.Tests/Tests/Simple/CalendarDayTestSuite.fs
@@ -10,9 +10,29 @@ open Zorglub.Testing.Facts.Simple
 open Zorglub.Time.Simple
 
 [<Sealed>]
+type ArmenianTests() =
+    inherit CalendarDayFacts<StandardArmenian12DataSet>(ArmenianCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type CopticTests() =
+    inherit CalendarDayFacts<StandardCoptic12DataSet>(CopticCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type EthiopicTests() =
+    inherit CalendarDayFacts<StandardEthiopic12DataSet>(EthiopicCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
 type GregorianTests() =
     inherit CalendarDayFacts<ProlepticGregorianDataSet>(GregorianCalendar.Instance, JulianCalendar.Instance)
 
 [<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
 type JulianTests() =
     inherit CalendarDayFacts<ProlepticJulianDataSet>(JulianCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type TabularIslamicTests() =
+    inherit CalendarDayFacts<StandardTabularIslamicDataSet>(TabularIslamicCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type ZoroastrianTests() =
+    inherit CalendarDayFacts<StandardZoroastrian12DataSet>(ZoroastrianCalendar.Instance, GregorianCalendar.Instance)
--- test/Zorglub.Tests/Tests/Simple/OrdinalDateTestSuite.fs
+++ test/Zorglub.Tests/Tests/Simple/OrdinalDateTestSuite.fs
@@ -10,9 +10,29 @@ open Zorglub.Testing.Facts.Simple
 open Zorglub.Time.Simple
 
 [<Sealed>]
+type ArmenianTests() =
+    inherit OrdinalDateFacts<StandardArmenian12DataSet>(ArmenianCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type CopticTests() =
+    inherit OrdinalDateFacts<StandardCoptic12DataSet>(CopticCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type EthiopicTests() =
+    inherit OrdinalDateFacts<StandardEthiopic12DataSet>(EthiopicCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
 type GregorianTests() =
     inherit OrdinalDateFacts<ProlepticGregorianDataSet>(GregorianCalendar.Instance, JulianCalendar.Instance)
 
 [<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
 type JulianTests() =
     inherit OrdinalDateFacts<ProlepticJulianDataSet>(JulianCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type TabularIslamicTests() =
+    inherit OrdinalDateFacts<StandardTabularIslamicDataSet>(TabularIslamicCalendar.Instance, GregorianCalendar.Instance)
+
+[<Sealed; TestExcludeFrom(TestExcludeFrom.Smoke)>]
+type ZoroastrianTests() =
+    inherit OrdinalDateFacts<StandardZoroastrian12DataSet>(ZoroastrianCalendar.Instance, GregorianCalendar.Instance)
