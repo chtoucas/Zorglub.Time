6c3db36738da29cfc86f3319f9d4e7a97ea6e03f
Chtoucas <chtoucas@narvalo.org>
2022-05-14 09:22:48 +0200

    Create CalendarTests.

---
+++ test/Zorglub.Tests/Tests/Simple/CalendarTests.fs
@@ -0,0 +1,55 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2020 Narvalo.Org. All rights reserved.
+
+module Zorglub.Tests.Simple.CalendarTests
+
+open System
+
+open Zorglub.Testing
+open Zorglub.Testing.Data
+open Zorglub.Testing.Data.Bounded
+
+open Zorglub.Time
+open Zorglub.Time.Simple
+
+open Xunit
+
+// NB: here, we test calendar-specific methods.
+// For methods related to IEpagomenalCalendar, this is done in FeaturetteTestSuite.
+
+module GregorianCase =
+    let private chr = GregorianCalendar.Instance
+    let private chrDataSet = ProlepticGregorianDataSet.Instance
+
+    let dayOfWeekData = chrDataSet.DayOfWeekData
+
+    [<Theory; MemberData(nameof(dayOfWeekData))>]
+    let ``GetDayOfWeek(CalendarDate)`` (info: YemodaAnd<DayOfWeek>) =
+        let (y, m, d, dayOfWeek) = info.Deconstruct()
+        let date = chr.GetCalendarDate(y, m, d)
+
+        chr.GetDayOfWeek(date) === dayOfWeek
+
+    [<Theory; MemberData(nameof(dayOfWeekData))>]
+    let ``GetDayOfWeek(OrdinalDate)`` (info: YemodaAnd<DayOfWeek>) =
+        let (y, m, d, dayOfWeek) = info.Deconstruct()
+        let date = chr.GetCalendarDate(y, m, d).ToOrdinalDate()
+
+        chr.GetDayOfWeek(date) === dayOfWeek
+
+module JulianCase =
+    let private chr = JulianCalendar.Instance
+
+    let dayOfWeekData = CalCalDataSet.DayOfWeekData
+
+    [<Theory; MemberData(nameof(dayOfWeekData))>]
+    let ``GetDayOfWeek(CalendarDate)`` (dayNumber: DayNumber) (dayOfWeek: DayOfWeek) =
+        let date = chr.GetCalendarDateOn(dayNumber)
+
+        chr.GetDayOfWeek(date) === dayOfWeek
+
+    [<Theory; MemberData(nameof(dayOfWeekData))>]
+    let ``GetDayOfWeek(OrdinalDate)`` (dayNumber: DayNumber) (dayOfWeek: DayOfWeek) =
+        let date = chr.GetOrdinalDateOn(dayNumber)
+
+        chr.GetDayOfWeek(date) === dayOfWeek
