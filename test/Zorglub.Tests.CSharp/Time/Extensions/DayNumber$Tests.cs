0b5f74d52e5435a28d575a26b86a79f001c88a0d
Chtoucas <chtoucas@narvalo.org>
2022-05-23 09:10:22 +0200

    Nada.

--- test/Zorglub.Tests.CSharp/Time/Extensions/DayNumber$Tests.cs
+++
@@ -1,7 +0,0 @@
-﻿// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2020 Narvalo.Org. All rights reserved.
-
-//
-// Fichier intentionnellement laissé vide.
-// Les tests pour DayNumberX sont dans DayNumberTests+DayOfWeek.cs
-//
