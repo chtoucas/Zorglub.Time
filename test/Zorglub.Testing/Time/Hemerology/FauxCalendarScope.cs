0a21ea3c1aa31866d680daec7c958a6aa9827336
Chtoucas <chtoucas@narvalo.org>
2022-07-15 10:24:06 +0200

    Update tests.

--- test/Zorglub.Testing/Time/Hemerology/FauxCalendarScope.cs
+++ test/Zorglub.Testing/Time/Hemerology/FauxCalendarScope.cs
@@ -14,6 +14,13 @@ public sealed class FauxCalendarScope : CalendarScope
     public FauxCalendarScope(ICalendricalSchema schema, DayNumber epoch, int minYear, int maxYear)
         : base(epoch, CalendricalSegment.Create(schema, Range.Create(minYear, maxYear))) { }
 
+    public FauxCalendarScope(DayNumber epoch, CalendricalSegment segment)
+        : base(epoch, segment) { }
+
+    // Constructor to be able to test the base constructors; see also WithMinDaysInXXX().
+    public FauxCalendarScope(CalendricalSegment segment)
+        : base(default, segment) { }
+
     public override void ValidateYearMonth(int year, int month, string? paramName = null) => throw new NotSupportedException();
     public override void ValidateYearMonthDay(int year, int month, int day, string? paramName = null) => throw new NotSupportedException();
     public override void ValidateOrdinal(int year, int dayOfYear, string? paramName = null) => throw new NotSupportedException();
--- test/Zorglub.Tests/Tests/Hemerology/Scopes/CalendarScopeTests.fs
+++ test/Zorglub.Tests/Tests/Hemerology/Scopes/CalendarScopeTests.fs
@@ -5,11 +5,53 @@ module Zorglub.Tests.Hemerology.Scopes.CalendarScopeTests
 
 open Zorglub.Testing
 
+open Zorglub.Time
+open Zorglub.Time.Core
+open Zorglub.Time.Core.Intervals
+open Zorglub.Time.Core.Schemas
 open Zorglub.Time.Hemerology
 
 open Xunit
 
+// TODO(test): test scope.Schema (protected internal).
+
 module Prelude =
     [<Fact>]
-    let ``Constructor throws when "schema" is null`` () =
-        nullExn "schema" (fun () -> new FauxCalendarScope(null, 1, 2))
+    let ``Constructor throws when "segment" is null`` () =
+        nullExn "segment" (fun () -> new FauxCalendarScope(null))
+
+    [<Fact>]
+    let ``Property Epoch`` () =
+        let epoch = DayZero.NewStyle + 123_456_789
+        let scope = new FauxCalendarScope(new FauxCalendricalSchema(), epoch, 1, 4)
+
+        scope.Epoch === epoch
+
+    [<Fact>]
+    let ``Property Domain`` () =
+        let epoch = DayZero.NewStyle + 123_456_789
+        let sch = new GregorianSchema()
+        let supportedYears = Range.Create(1, 4)
+        let scope = new FauxCalendarScope(epoch, CalendricalSegment.Create(sch, supportedYears))
+
+        let minDayNumber = epoch + sch.GetStartOfYear(supportedYears.Min)
+        let maxDayNumber = epoch + sch.GetEndOfYear(supportedYears.Max)
+        let range = Range.Create(minDayNumber, maxDayNumber)
+
+        scope.Domain === range
+
+    [<Fact>]
+    let ``Property Segment`` () =
+        let sch = new GregorianSchema()
+        let seg = CalendricalSegment.Create(sch, Range.Create(1, 4))
+        let scope = new FauxCalendarScope(seg)
+
+        scope.Segment ==& seg
+
+    [<Fact>]
+    let ``Property Schema`` () =
+        let sch = new GregorianSchema()
+        let seg = CalendricalSegment.Create(sch, Range.Create(1, 4))
+        let scope = new FauxCalendarScope(seg) :> ISchemaBound
+
+        scope.Schema ==& sch
