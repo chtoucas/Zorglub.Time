4aa9a65996a7447d99d6b328d64bc99bd2512495
Chtoucas <chtoucas@narvalo.org>
2022-07-20 08:14:19 +0200

    Update NamespaceDoc.cs

--- src/Zorglub.Sketches/Time/Geometry/NamespaceDoc.cs
+++ src/Zorglub.Sketches/Time/Geometry/NamespaceDoc.cs
@@ -4,11 +4,10 @@
 namespace Zorglub.Time.Geometry
 {
     /// <summary>
-    /// The <see cref="Geometry"/> namespace contains schema templates for
-    /// calendars sufficiently regular to be studied with means from Discrete
-    /// Geometry.
-    /// <para>Classes in this namespace should only be used when trying to
-    /// decipher calendrical formulae.</para>
+    /// The <see cref="Geometry"/> namespace contains schema templates for calendars sufficiently
+    /// regular to be studied with means from Discrete Geometry.
+    /// <para>Classes in this namespace should only be used when trying to decipher calendrical
+    /// formulae.</para>
     /// </summary>
     [CompilerGenerated]
     internal static class NamespaceDoc { }
