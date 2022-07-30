08dce7f750e284d26f353cb0187543f8c697a8a6
Chtoucas <chtoucas@narvalo.org>
2022-07-30 08:31:24 +0200

    Re-organize files.

--- test/Zorglub.Tests/Tests/Extras/GregorianReformTests.fs
+++ test/Zorglub.Tests/Tests/Extras/GregorianReformTests.fs
@@ -5,25 +5,49 @@ module Zorglub.Tests.Time.Extras.GregorianReformTests
 
 open Zorglub.Testing
 
-open Zorglub.Time.Extras
-open Zorglub.Time.Specialized
-
 open Xunit
 
-[<Fact>]
-let ``Property Official.SecularShift`` () =
-    GregorianReform.Official.SecularShift === 10
+module Specialized =
+    open Zorglub.Time.Extras
+    open Zorglub.Time.Specialized
 
-[<Fact>]
-let ``Property Official (from last Julian date)`` () =
-    let date = new JulianDate(1582, 10, 4)
-    let reform = GregorianReform.FromLastJulianDate(date)
+    [<Fact>]
+    let ``Property Official.SecularShift`` () =
+        GregorianReform.Official.SecularShift === 10
 
-    GregorianReform.Official === reform
+    [<Fact>]
+    [<TestExcludeFrom(TestExcludeFrom.Regular)>]
+    let ``Property Official (from last Julian date)`` () =
+        let date = new JulianDate(1582, 10, 4)
+        let reform = GregorianReform.FromLastJulianDate(date)
 
-[<Fact>]
-let ``Property Official (from first Gregorian date)`` () =
-    let date = new GregorianDate(1582, 10, 15)
-    let reform = GregorianReform.FromFirstGregorianDate(date)
+        GregorianReform.Official === reform
 
-    GregorianReform.Official === reform
+    [<Fact>]
+    [<TestExcludeFrom(TestExcludeFrom.Regular)>]
+    let ``Property Official (from first Gregorian date)`` () =
+        let date = new GregorianDate(1582, 10, 15)
+        let reform = GregorianReform.FromFirstGregorianDate(date)
+
+        GregorianReform.Official === reform
+
+module Simple =
+    open Zorglub.Time.Simple
+
+    [<Fact>]
+    let ``Property Official.SecularShift`` () =
+        GregorianReform.Official.SecularShift === 10
+
+    [<Fact>]
+    [<TestExcludeFrom(TestExcludeFrom.Regular)>]
+    let ``Property Official (from last Julian date)`` () =
+        let reform = GregorianReform.FromLastJulianDate(1582, 10, 4)
+
+        GregorianReform.Official === reform
+
+    [<Fact>]
+    [<TestExcludeFrom(TestExcludeFrom.Regular)>]
+    let ``Property Official (from first Gregorian date)`` () =
+        let reform = GregorianReform.FromFirstGregorianDate(1582, 10, 15)
+
+        GregorianReform.Official === reform
