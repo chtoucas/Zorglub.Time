0c2cdbff48b528b487f5314b14ba1e6a0c3c7dc6
Chtoucas <policromia@gmail.com>
2022-08-01 13:53:27 +0200

    Create IBlankDay<>.

---
+++ src/Zorglub/Time/Hemerology/IBlankDay.cs
@@ -0,0 +1,17 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2020 Narvalo.Org. All rights reserved.
+
+namespace Zorglub.Time.Hemerology
+{
+    /// <summary>
+    /// Defines methods specific to calendars featuring epagomenal days.
+    /// <para>The epagomenal days are usually found in descendants of the Egyptian calendar.</para>
+    /// </summary>
+    public interface IBlankDay<TSelf> : IDateable where TSelf : IBlankDay<TSelf>
+    {
+        /// <summary>
+        /// Returns true if the current instance is a blank day; otherwise returns false.
+        /// </summary>
+        bool IsBlank { get; }
+    }
+}
