ff25c5cb4803292b9d7e876728b0357bc96e1ec9
Chtoucas <chtoucas@narvalo.org>
2022-08-03 09:08:52 +0200

    Update MinMaxYearScopeTests.fs
    [skip ci]

--- test/Zorglub.Tests/Tests/Hemerology/Scopes/MinMaxYearScopeTests.fs
+++ test/Zorglub.Tests/Tests/Hemerology/Scopes/MinMaxYearScopeTests.fs
@@ -33,11 +33,78 @@ module Factories =
         argExn "supportedYears" (fun () -> MinMaxYearScope.Create(sch, DayZero.NewStyle, Range.Create(11, 12)))
 
     [<Fact>]
+    let ``Create()`` () =
+        let range = Range.Create(-2, 9)
+        let sch = new FauxSystemSchema(Range.Create(-9, 10))
+        let scope = MinMaxYearScope.Create(sch, DayZero.NewStyle, range)
+
+        scope.Segment.SupportedYears === range
+
+    [<Fact>]
     let ``CreateMaximal() throws for null schema`` () =
         nullExn "schema" (fun () -> MinMaxYearScope.CreateMaximal(null, DayZero.NewStyle))
-        nullExn "schema" (fun () -> MinMaxYearScope.CreateMaximal(null, DayZero.NewStyle))
+
+    [<Fact>]
+    let ``CreateMaximal()`` () =
+        let range = Range.Create(-10, 10)
+        let sch = new FauxSystemSchema(range)
+        let scope = MinMaxYearScope.CreateMaximal(sch, DayZero.NewStyle)
+
+        scope.Segment.SupportedYears === range
 
     [<Fact>]
     let ``CreateMaximalOnOrAfterYear1() throws for null schema`` () =
         nullExn "schema" (fun () -> MinMaxYearScope.CreateMaximalOnOrAfterYear1(null, DayZero.NewStyle))
-        nullExn "schema" (fun () -> MinMaxYearScope.CreateMaximalOnOrAfterYear1(null, DayZero.NewStyle))
+
+    [<Fact>]
+    let ``CreateMaximalOnOrAfterYear1() throws when the schema only supports years <= 0`` () =
+        let range = Range.Create(-10, 0)
+        let sch = new FauxSystemSchema(range)
+
+        argExn "schema" (fun () -> MinMaxYearScope.CreateMaximalOnOrAfterYear1(sch, DayZero.NewStyle))
+
+    [<Fact>]
+    let ``CreateMaximalOnOrAfterYear1()`` () =
+        let range = Range.Create(-10, 10)
+        let sch = new FauxSystemSchema(range)
+        let scope = MinMaxYearScope.CreateMaximalOnOrAfterYear1(sch, DayZero.NewStyle)
+
+        scope.Segment.SupportedYears === Range.Create(1, 10)
+
+    [<Fact>]
+    let ``StartingAt() throws for null schema`` () =
+        nullExn "schema" (fun () -> MinMaxYearScope.StartingAt(null, DayZero.NewStyle, 1))
+
+    [<Fact>]
+    let ``StartingAt() throws when the year is out of range`` () =
+        let range = Range.Create(2, 10)
+        let sch = new FauxSystemSchema(range)
+
+        outOfRangeExn "year" (fun () -> MinMaxYearScope.StartingAt(sch, DayZero.NewStyle, 1))
+
+    [<Fact>]
+    let ``StartingAt()`` () =
+        let range = Range.Create(2, 10)
+        let sch = new FauxSystemSchema(range)
+        let scope = MinMaxYearScope.StartingAt(sch, DayZero.NewStyle, 3)
+
+        scope.Segment.SupportedYears === Range.Create(3, 10)
+
+    [<Fact>]
+    let ``EndingAt() throws for null schema`` () =
+        nullExn "schema" (fun () -> MinMaxYearScope.EndingAt(null, DayZero.NewStyle, 1))
+
+    [<Fact>]
+    let ``EndingAt() throws when the year is out of range`` () =
+        let range = Range.Create(2, 10)
+        let sch = new FauxSystemSchema(range)
+
+        outOfRangeExn "year" (fun () -> MinMaxYearScope.EndingAt(sch, DayZero.NewStyle, 1))
+
+    [<Fact>]
+    let ``EndingAt()`` () =
+        let range = Range.Create(2, 10)
+        let sch = new FauxSystemSchema(range)
+        let scope = MinMaxYearScope.EndingAt(sch, DayZero.NewStyle, 5)
+
+        scope.Segment.SupportedYears === Range.Create(2, 5)
