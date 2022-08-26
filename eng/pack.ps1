18e649a3673556a9f2da0a76d425e963fa96493c
Chtoucas <chtoucas@narvalo.org>
2022-08-26 07:59:00 +0200

    Update packaging script to pack Zorglub.Time.Extras too.
    [skip ci]

--- eng/pack.ps1
+++ eng/pack.ps1
@@ -3,23 +3,65 @@
 #Requires -Version 7
 
 [CmdletBinding()]
-param()
+param(
+                 [switch] $NoExtras,
+
+    [Alias('h')] [switch] $Help
+)
 
 . (Join-Path $PSScriptRoot 'zorglub.ps1')
+
+#-------------------------------------------------------------------------------
+
+function Print-Help {
+    say @"
+
+Packaging script.
+
+Usage: pack.ps1 [arguments]
+     -NoExtras       do NOT pack Zorglub.Time.Extras?
+  -h|-Help           print this help then exit
+
+"@
+}
+
+#-------------------------------------------------------------------------------
+
+if ($Help) { Print-Help ; exit }
 
 try {
     pushd $RootDir
 
-    $project = Join-Path $SrcDir 'Zorglub' -Resolve
-    $packageDir = Join-Path $ArtifactsDir 'packages'
+    $args = "-c:Release",
+        "/p:ContinuousIntegrationBuild=true",
+        "/p:HideInternals=true",
+        "/p:PrintSettings=true"
 
-    & dotnet pack $project `
-        -c Release `
-        -o $packageDir `
-        /p:ContinuousIntegrationBuild=true `
-        /p:HideInternals=true `
-        /p:PrintSettings=true
-        || die 'Failed to pack the project.'
+    $mainProject   = Join-Path $SrcDir 'Zorglub' -Resolve
+    $extrasProject = Join-Path $SrcDir 'Zorglub.Extras' -Resolve
+
+    say 'Cleaning solution...' -Foreground Magenta
+    & dotnet clean -c Release -v minimal
+
+    say "`nBuilding project Zorglub.Extras..." -Foreground Magenta
+    # Safety measures:
+    # - Always build Zorglub.Extras (HideInternals=true is "unsafe")
+    # - Delete project.assets.json (--force)
+    & dotnet build $extrasProject $args --force
+
+    say "`nPackaging Zorglub.Time..." -Foreground Magenta
+    # Pack Zorglub.Time
+    & dotnet pack $mainProject $args --no-build --output $PackagesDir `
+        || die "Failed to pack '$mainProject'."
+
+    say "`nPackaging Zorglub.Time.Extras..." -Foreground Magenta
+    # Pack Zorglub.Time.Extras
+    if (-not $NoExtras) {
+        & dotnet pack $extrasProject $args --no-build --output $PackagesDir `
+            || die "Failed to pack '$extrasProject'."
+    }
+
+    say "`nPackaging completed successfully" -Foreground Green
 }
 finally {
     popd
--- eng/zorglub.ps1
+++ eng/zorglub.ps1
@@ -6,6 +6,7 @@ New-Variable RootDir (Get-Item $PSScriptRoot).Parent.FullName -Scope Script -Opt
 New-Variable SrcDir       (Join-Path $RootDir 'src')  -Scope Script -Option Constant
 New-Variable TestDir      (Join-Path $RootDir 'test') -Scope Script -Option Constant
 New-Variable ArtifactsDir (Join-Path $RootDir '__')   -Scope Script -Option Constant
+New-Variable PackagesDir  (Join-Path $ArtifactsDir 'packages')   -Scope Script -Option Constant
 
 New-Variable TestProject (Join-Path $TestDir "Zorglub.Tests") -Scope Script -Option Constant
 
