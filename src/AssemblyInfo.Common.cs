686e37c5a60a1b4da8ee53ca16fdfc0b1da06515
Chtoucas <policromia@gmail.com>
2022-09-24 17:26:18 +0200

    Update project files.

--- src/AssemblyInfo.Common.cs
+++ src/AssemblyInfo.Common.cs
@@ -7,9 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 #if __ENABLE_PREVIEW_FEATURES__
-
-//[assembly: SuppressMessage("Usage", "CA2252:This API requires opting into preview features")]
-
 #warning Built using preview features of the .NET platform
-
 #endif
