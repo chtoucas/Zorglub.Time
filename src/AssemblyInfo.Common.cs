6f620507808ec36a099c7b01194f3737acd3614d
Chtoucas <policromia@gmail.com>
2022-09-24 15:39:59 +0200

    Clean up.

--- src/AssemblyInfo.Common.cs
+++ src/AssemblyInfo.Common.cs
@@ -5,3 +5,11 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
+
+#if __ENABLE_PREVIEW_FEATURES__
+
+//[assembly: SuppressMessage("Usage", "CA2252:This API requires opting into preview features")]
+
+#warning Built using preview features of the .NET platform
+
+#endif
