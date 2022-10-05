ae9e0b68646f3c1e93859ed8df34667b4ccab7c5
Chtoucas <chtoucas@narvalo.org>
2022-10-05 09:14:56 +0200

    Optimize a bit JulianDate.

--- src/Zorglub/Time/Core/Schemas/JulianFormulae.cs
+++ src/Zorglub/Time/Core/Schemas/JulianFormulae.cs
@@ -9,6 +9,10 @@ namespace Zorglub.Time.Core.Schemas;
 /// </summary>
 internal static class JulianFormulae
 {
+    /// <summary>Determines whether the specified date is an intercalary day or not.</summary>
+    [Pure]
+    public static bool IsIntercalaryDay(int m, int d) => m == 2 && d == 29;
+
     /// <summary>Determines whether the specified year is leap or not.</summary>
     [Pure]
     // CIL code size = 9 bytes <= 32 bytes.
@@ -45,13 +49,33 @@ internal static class JulianFormulae
         : IsLeapYear(y) ? 29
         : 28;
 
-    // WARNING: si on ajoute des versions 32-bit, il faudra sûrement vérifier que cela
+    // TODO(code): si on ajoute des versions 32-bit, il faudra sûrement vérifier que cela
     // n'entraînera pas de dépassements arithmétiques dans DayNumber.
 
     /// <summary>Counts the number of consecutive days from the epoch to the specified date.
     /// </summary>
     [Pure]
     public static long CountDaysSinceEpoch(long y, int m, int d)
+    {
+        if (m < 3)
+        {
+            y--;
+            m += 9;
+        }
+        else
+        {
+            m -= 3;
+        }
+
+        return -GJSchema.DaysInYearAfterFebruary
+            + (JulianSchema.DaysPer4YearCycle * y >> 2)
+            + (int)((uint)(153 * m + 2) / 5) + d - 1;
+    }
+
+    /// <summary>Counts the number of consecutive days from the epoch to the specified date.
+    /// </summary>
+    [Pure]
+    public static int CountDaysSinceEpoch(int y, int m, int d)
     {
         if (m < 3)
         {
@@ -91,10 +115,39 @@ internal static class JulianFormulae
         }
     }
 
+    /// <summary>Obtains the date parts for the specified day count (the number of consecutive days
+    /// from the epoch to a date); the results are given in output parameters.</summary>
+    public static void GetDateParts(int daysSinceEpoch, out int y, out int m, out int d)
+    {
+        daysSinceEpoch += GJSchema.DaysInYearAfterFebruary;
+
+        y = MathZ.Divide((daysSinceEpoch << 2) + 3, JulianSchema.DaysPer4YearCycle);
+        int d0y = daysSinceEpoch - (JulianSchema.DaysPer4YearCycle * y >> 2);
+
+        m = (int)((uint)(5 * d0y + 2) / 153);
+        d = 1 + d0y - (int)((uint)(153 * m + 2) / 5);
+
+        if (m > 9)
+        {
+            y++;
+            m -= 9;
+        }
+        else
+        {
+            m += 3;
+        }
+    }
+
     /// <summary>Obtains the year from the specified day count (the number of consecutive days from
     /// the epoch to a date).</summary>
     [Pure]
     public static long GetYear(long daysSinceEpoch) =>
+        MathZ.Divide((daysSinceEpoch << 2) + 1464, JulianSchema.DaysPer4YearCycle);
+
+    /// <summary>Obtains the year from the specified day count (the number of consecutive days from
+    /// the epoch to a date).</summary>
+    [Pure]
+    public static int GetYear(int daysSinceEpoch) =>
         MathZ.Divide((daysSinceEpoch << 2) + 1464, JulianSchema.DaysPer4YearCycle);
 
     /// <summary>Counts the number of consecutive days from the epoch to the first day of the
---
+++ src/Zorglub/Time/Specialized/_JulianDate.cs
@@ -0,0 +1,159 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2020 Narvalo.Org. All rights reserved.
+
+namespace Zorglub.Time.Specialized;
+
+using Zorglub.Time.Core.Intervals;
+using Zorglub.Time.Core.Schemas;
+using Zorglub.Time.Core.Validation;
+using Zorglub.Time.Hemerology;
+using Zorglub.Time.Hemerology.Scopes;
+
+public partial struct JulianDate
+{
+    // WARNING: the order in which the static fields are written is __important__.
+
+    private static readonly JulianSchema s_Schema = new();
+    private static readonly JulianCalendar s_Calendar = new(s_Schema);
+    private static readonly MinMaxYearScope s_Scope = s_Calendar.Scope;
+    private static readonly DayNumber s_Epoch = s_Calendar.Epoch;
+    private static readonly Range<DayNumber> s_Domain = s_Calendar.Domain;
+    private static readonly JulianAdjuster s_Adjuster = new(s_Scope);
+    private static readonly JulianDate s_MinValue = new(s_Domain.Min - s_Epoch);
+    private static readonly JulianDate s_MaxValue = new(s_Domain.Max - s_Epoch);
+
+    private readonly int _daysSinceEpoch;
+
+    /// <summary>Initializes a new instance of the <see cref="JulianDate"/> struct to the specified date parts.</summary>
+    /// <exception cref="AoorException">The specified components do not form a valid date or
+    /// <paramref name="year"/> is outside the range of supported years.</exception>
+    public JulianDate(int year, int month, int day)
+    {
+        s_Scope.ValidateYearMonthDay(year, month, day);
+
+        _daysSinceEpoch = JulianFormulae.CountDaysSinceEpoch(year, month, day);
+    }
+
+    /// <summary>Initializes a new instance of the <see cref="JulianDate"/> struct to the specified ordinal date parts.</summary>
+    /// <exception cref="AoorException">The specified components do not form a valid ordinal date or
+    /// <paramref name="year"/> is outside the range of supported years.</exception>
+    public JulianDate(int year, int dayOfYear)
+    {
+        s_Scope.ValidateOrdinal(year, dayOfYear);
+
+        _daysSinceEpoch = s_Schema.CountDaysSinceEpoch(year, dayOfYear);
+    }
+
+    /// <summary>Initializes a new instance of the <see cref="JulianDate"/> struct to the specified day number.</summary>
+    /// <exception cref="AoorException"><paramref name="dayNumber"/> is outside the range of supported values.</exception>
+    public JulianDate(DayNumber dayNumber)
+    {
+        s_Domain.Validate(dayNumber);
+
+        _daysSinceEpoch = dayNumber - s_Epoch;
+    }
+
+    /// <summary>This constructor does NOT validate its parameter.</summary>
+    internal JulianDate(int daysSinceEpoch)
+    {
+        _daysSinceEpoch = daysSinceEpoch;
+    }
+
+    /// <inheritdoc />
+    /// <remarks>This static property is thread-safe.</remarks>
+    public static JulianDate MinValue => s_MinValue;
+
+    /// <inheritdoc />
+    /// <remarks>This static property is thread-safe.</remarks>
+    public static JulianDate MaxValue => s_MaxValue;
+
+    /// <summary>Gets the date adjuster.
+    /// <para>This static property is thread-safe.</para></summary>
+    public static JulianAdjuster Adjuster => s_Adjuster;
+
+    /// <inheritdoc />
+    public static JulianCalendar Calendar => s_Calendar;
+
+    /// <inheritdoc />
+    public DayNumber DayNumber => s_Epoch + _daysSinceEpoch;
+
+    /// <inheritdoc />
+    public int DaysSinceEpoch => _daysSinceEpoch;
+
+    /// <inheritdoc />
+    public Ord CenturyOfEra => Ord.FromInt32(Century);
+
+    /// <inheritdoc />
+    public int Century => YearNumbering.GetCentury(Year);
+
+    /// <inheritdoc />
+    public Ord YearOfEra => Ord.FromInt32(Year);
+
+    /// <inheritdoc />
+    public int YearOfCentury => YearNumbering.GetYearOfCentury(Year);
+
+    /// <inheritdoc />
+    public int Year => JulianFormulae.GetYear(_daysSinceEpoch);
+
+    /// <inheritdoc />
+    public int Month
+    {
+        get
+        {
+            JulianFormulae.GetDateParts(_daysSinceEpoch, out _, out int m, out _);
+            return m;
+        }
+    }
+
+    /// <inheritdoc />
+    public int DayOfYear
+    {
+        get
+        {
+            _ = s_Schema.GetYear(_daysSinceEpoch, out int doy);
+            return doy;
+        }
+    }
+
+    /// <inheritdoc />
+    public int Day
+    {
+        get
+        {
+            JulianFormulae.GetDateParts(_daysSinceEpoch, out _, out _, out int d);
+            return d;
+        }
+    }
+
+    /// <inheritdoc />
+    public DayOfWeek DayOfWeek => DayNumber.DayOfWeek;
+
+    /// <inheritdoc />
+    public bool IsIntercalary
+    {
+        get
+        {
+            JulianFormulae.GetDateParts(_daysSinceEpoch, out _, out int m, out int d);
+            return JulianFormulae.IsIntercalaryDay(m, d);
+        }
+    }
+
+    /// <inheritdoc />
+    public bool IsSupplementary => false;
+
+    /// <summary>Returns a culture-independent string representation of the current instance.</summary>
+    [Pure]
+    public override string ToString()
+    {
+        JulianFormulae.GetDateParts(_daysSinceEpoch, out int y, out int m, out int d);
+        return FormattableString.Invariant($"{d:D2}/{m:D2}/{y:D4} ({s_Calendar})");
+    }
+
+    /// <inheritdoc />
+    public void Deconstruct(out int year, out int month, out int day) =>
+        JulianFormulae.GetDateParts(_daysSinceEpoch, out year, out month, out day);
+
+    /// <inheritdoc />
+    public void Deconstruct(out int year, out int dayOfYear) =>
+        year = s_Schema.GetYear(_daysSinceEpoch, out dayOfYear);
+}
