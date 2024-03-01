ec66d0b6e8746cd2ff539d370eb588a1b8c7c750
Chtoucas <policromia@gmail.com>
2024-03-01 07:14:44 +0100

    Remove unnecessary pragmas.

--- src/Play/Program.cs
+++ src/Play/Program.cs
@@ -1,9 +1,6 @@
 ﻿// SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2020 Narvalo.Org. All rights reserved.
 
-#pragma warning disable CA1812 // Avoid uninstantiated internal classes (Performance)
-#pragma warning disable CA1852
-
 using System;
 
 using Play.Demo;
--- test/Zorglub.Testing/Usings.cs
+++ test/Zorglub.Testing/Usings.cs
@@ -1,9 +1,6 @@
 ﻿// SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2020 Narvalo.Org. All rights reserved.
 
-#pragma warning disable IDE0065 // Misplaced using directive (Style)
-// A global using directive cannot be used inside a namespace declaration.
-
 global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics;
